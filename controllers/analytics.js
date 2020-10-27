4fb8adfc563118eb946280d4dc4260e2a76a4518
Ivan <ivan.zf.netc@gmail.com>
2020-10-27 17:24:07 +0200

    added angular client

--- controllers/analytics.js
+++ controllers/analytics.js
@@ -1,11 +1,7 @@
 module.exports.overview = function(request, response){
-  /* response.status(200).json({
-    login: 'login from controller'
-  }) */
+  
 };
 
 module.exports.analytics = function(request, response){
-  /* response.status(200).json({
-    register: 'register from controller'
-  }) */
+  
 };
