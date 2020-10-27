e3c806228d49ceaaac96f6e1cf7754c79bc2e1e9
Ivan <ivan.zf.netc@gmail.com>
2020-10-27 09:26:29 +0200

    finished avtorisation and registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,31 @@
+const bcrypt = require('bcryptjs');
+const { request } = require('express');
+const jsonWebToken = require('jsonwebtoken');
+const keys = require('../config/keys');
 const User = require('../models/User');
 
-module.exports.login = function(request, response){
-  response.status(200).json({
-    login: {
-      email: request.body.email,
-      password: request.body.password
+
+module.exports.login = async function(request, response){
+  const candidate = await User.findOne({email: request.body.email});
+  if(candidate){
+    //if passwords ident 
+    const passwordResult = bcrypt.compareSync(request.body.password, candidate.password);
+    if(passwordResult){
+      //generate token
+      const token = jsonWebToken.sign({
+        email: candidate.email,
+        userId: candidate._id
+      }, keys.jsonWebToken, {expiresIn: 60*60});
+      response.status(200).json({
+        token: `Bearer ${token}`
+      })
+    } else {
+      response.status(401).json({message: 'password is invalid'});
     }
-  })
+  } else {
+    //error 
+    response.status(404).json({message: 'user with this email not found'})
+  }
 };
 
 module.exports.register = async function(request, response){
@@ -19,10 +38,12 @@ module.exports.register = async function(request, response){
       message: 'Try enother email. This email is occuped!'
     })
   } else {
-    //user is not found
+    //user is not found, we need to create it
+    const salt = bcrypt.genSaltSync(10);
+    const password = request.body.password;
    const user = new User({
      email: request.body.email,
-     password: request.body.password
+     password: bcrypt.hashSync(password, salt)
    });
 
    try{
@@ -31,6 +52,5 @@ module.exports.register = async function(request, response){
    } catch(error){
      console.log(error);
    }
-   
   }
 };
